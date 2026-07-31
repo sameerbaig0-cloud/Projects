ac594aad6a46719e97de8e020e26be649d213250
sameerbaig0-cloud <sameerbaig0@gmail.com>
2026-08-01 01:36:09 +0530

    Update

---
+++ Haier Service Center/WEB_PROJECT/Models/ItemMaster.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ServicePlatform.Models
+{
+    [Table("ItemMaster", Schema = "dbo")]
+    public partial class ItemMaster
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long ItemID { get; set; }
+
+        public string ItemCode { get; set; }
+
+        public string ItemName { get; set; }
+
+        public long UnitID { get; set; }
+
+        public string CreatedDateTime { get; set; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
